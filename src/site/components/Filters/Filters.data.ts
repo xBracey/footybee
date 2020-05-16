dcf8b3f8706d6231892be08e8ecf949cd4d5a5ca
Tom <tom@Hangmans-iMac.local>
2020-05-16 10:59:33 +0100

    Add extra years

--- src/site/components/Filters/Filters.data.ts
+++ src/site/components/Filters/Filters.data.ts
@@ -1,5 +1,21 @@
 export const yearOptions = [
   {
+    value: "2011",
+    label: "2011",
+  },
+  {
+    value: "2012",
+    label: "2012",
+  },
+  {
+    value: "2013",
+    label: "2013",
+  },
+  {
+    value: "2014",
+    label: "2014",
+  },
+  {
     value: "2015",
     label: "2015",
   },
