cd9e2dbe05f87f883f1eb8b23e848fdadacd8e66
Thomas Brace <tom@192.168.1.250>
2021-06-10 18:41:52 +0100

    Fix build

--- src/site/components/Header/Header.stories.tsx
+++ src/site/components/Header/Header.stories.tsx
@@ -5,8 +5,10 @@ import { headerData } from "data";
 
 const story = storiesOf("Components/Header", module);
 
-story.add("Header Component", () => <Header menu={headerData(false)} />);
+story.add("Header Component", () => (
+  <Header menu={headerData(false, "test")} />
+));
 
 story.add("Header Component is admin", () => (
-  <Header menu={headerData(true)} />
+  <Header menu={headerData(true, "test")} />
 ));
