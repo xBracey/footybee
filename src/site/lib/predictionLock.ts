92ff0d7fe2ecdb4dae8c35cef5d7466ef33502b1
Thomas Brace <thomas.brace@titanacademy.io>
2022-09-26 08:39:59 +0100

    Fix lock time

--- src/site/lib/predictionLock.ts
+++ src/site/lib/predictionLock.ts
@@ -4,12 +4,12 @@ const usernames = [];
 
 export const predictionLock = (username: string) => {
   return (
-    moment().isAfter(moment.unix(1623436200)) && !usernames.includes(username)
+    moment().isAfter(moment.unix(1668902400)) && !usernames.includes(username)
   );
 };
 
 export const teamPredictionLock = (username: string) => {
   return (
-    moment().isAfter(moment.unix(1624722959)) && !usernames.includes(username)
+    moment().isAfter(moment.unix(1668902400)) && !usernames.includes(username)
   );
 };
