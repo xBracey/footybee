a0a051ca8abca50c21cb5036c97f6d370afe7481
Thomas Brace <thomas.brace@titanacademy.io>
2022-11-24 10:53:46 -0500

    Fix types

--- src/site/components/Table/PredictionsBreakdown/PredictionsBreakdown.stories.tsx
+++ src/site/components/Table/PredictionsBreakdown/PredictionsBreakdown.stories.tsx
@@ -10,24 +10,28 @@ const fixtures = [
     score: "2-1",
     prediction: "2-1",
     points: 25,
+    date: "2022-11-23T19:00:00.000Z",
   },
   {
     fixture: "England vs Croatia",
     score: "2-1",
     prediction: "1-0",
     points: 15,
+    date: "2022-11-22T19:00:00.000Z",
   },
   {
     fixture: "England vs Croatia",
     score: "2-1",
     prediction: "1-1",
     points: 0,
+    date: "2022-11-21T16:00:00.000Z",
   },
   {
     fixture: "England vs Croatia",
     score: "2-1",
-    prediction: "1-1",
+    prediction: "6-6",
     points: 0,
+    date: "2022-11-21T19:00:00.000Z",
   },
 ];
 
